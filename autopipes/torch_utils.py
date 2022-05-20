2a1fca042e3d19f099e16d242f7ee7e44f2a2f32
Leif Van Holland <leifvan.holland@gmx.de>
2022-05-20 10:13:27 +0200

    made CUDAMoveable runtime checkable

--- autopipes/torch_utils.py
+++ autopipes/torch_utils.py
@@ -1,6 +1,7 @@
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 
 
+@runtime_checkable
 class CUDAMoveable(Protocol):
     """
     A protocol for objects that provide ``.cpu()`` and ``.cuda()`` methods to move PyTorch tensors
